689e5e1bb9d53586284db2bbcc59a6c91045dd61
Dxun265 <lihua@atomicmail.io>
2025-07-28 16:49:24 +0800

    feat: dog shit code

---
+++ monica-rs/src/app/core/request.rs
@@ -0,0 +1,7 @@
+pub fn get(url: &str) -> String {}
+
+pub fn post(url: &str, data: std::collections::HashMap<String, String>) -> String {}
+
+pub fn delete(url: &str, data: std::collections::HashMap<String, String>) -> String {}
+
+pub fn connect(addr: &str) -> Result<()> {}
